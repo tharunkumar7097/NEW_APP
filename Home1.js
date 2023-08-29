ebc31e6b79316e1991262ea25f381d647da7e046
tharunkumar7097 <119399188+tharunkumar7097@users.noreply.github.com>
2023-08-29 22:47:47 +0530

    Add files via upload

---
+++ Home1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Home1 = () => {
+    return(
+        <div>Home1</div>
+    );
+}
+export default Home1;
---
+++ Home3.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Home3 = () => {
+    return(
+        <div>Home3</div>
+    );
+}
+export default Home3;
