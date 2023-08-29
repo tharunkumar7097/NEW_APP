53ea0e704558ed425cb6af6b1cecb1136e541c0a
tharunkumar7097 <119399188+tharunkumar7097@users.noreply.github.com>
2023-08-29 22:49:20 +0530

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Section1 from '../components/About/Section1'
+import Section2 from '../components/About/Section2'
+import Section3 from '../components/About/Section3'
+
+
+const About = () => {
+    return <>
+    <div>
+            <Section1/>
+            <Section2/>
+            <Section3/>
+    </div>
+    </>
+}
+export default About
---
+++ Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Home1 from '../components/Home/Home1'
+import Home2 from '../components/Home/Home2'
+import Home3 from '../components/Home/Home3'
+
+const Home = () => {
+    return <>
+        <div>
+            <Home1/>
+            <Home2/>
+            <Home3/>
+        </div>
+    </>
+
+}
+export default Home
